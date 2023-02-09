914abb3a971c1f4aa663d336f04f8f453eb551d6
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-02-09 13:23:27 +0000

    Update 09/02/2023 (Ultra Clickers Simulator)
    Added Support for Advanced Game Loader

--- Games/6512923934.lua
+++ Games/6512923934.lua
@@ -1,90 +1,73 @@
---Vars
-local inf = math.huge; local defaultNum = 50; local plr = game.Players.LocalPlayer; local char = plr.Character
-
---Ultra Clickers Simulator
 local functionNum = 0.00000000000000005
-print('Loading NovalineHub')
-local NovalineConnection = loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/mercury-plus/main/mercury-plus.lua"))()
+    local NovalineConnection = loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/mercury-plus/main/mercury-plus.lua"))()
+    local Novaline = NovalineConnection:create{
+		Name = 'NovalineHub',
+        Theme = NovalineConnection.Themes.Dark
+    }
+    local AutoFarmTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "AutoFarms"
+    }
+    local WorldsTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "World"
+    }
+    local EggsTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Eggs"
+    }
+    local MiscTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Misc"
+    }
 
-local Novaline = NovalineConnection:create{
-    Name = 'NovalineHub',
-    Theme = NovalineConnection.Themes.Dark
-}
+    _G.autoClicker = true
 
-local AutoFarmTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "AutoFarms"
-}
-
-local WorldsTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "World"
-}
-
-local EggsTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "Eggs"
-}
-
-local MiscTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "Misc"
-}
-
-_G.autoClicker = true
-
-function autoClick()
-    while _G.autoClicker do
-        workspace.Events.AddClick:FireServer()
-        wait(functionNum)
+    function autoClick()
+        while _G.autoClicker do
+            workspace.Events.AddClick:FireServer()
+            wait(functionNum)
+        end
     end
-end
-
-AutoFarmTab:Toggle{
-    Name = "AutoClicker",
-    StartingState = false,
-    Description = "Clicks for you",
-    Callback = function(state) 
-        _G.autoClicker = state
-        autoClick()
-    end
-}
-
-
-MiscTab:Button{
-    Name = "Reset Character",
-    Description = "Resets your character",
-    Callback = function() 
-        wait(0.05)
-        char.Humanoid.Health = 0
-    end
-}
-
-MiscTab:Button{
-    Name = "Kill Roblox",
-    Description = "Destroys roblox instance (just do alt+f4 smh)",
-    Callback = function() 
-        wait(1)
-        game:Shutdown()
-    end
-}
-
-MiscTab:Button{
-    Name = "Destroy NovalineHub",
-    Description = "whyyy :(((!!!",
-    Callback = function() 
-        wait(1)
-        Novaline:Destroy()
-    end
-}
-
-MiscTab:Slider{
-    Name = "Zoom",
-    Default = 130,
-    Min = 1,
-    Max = 10000,
-    Callback = function(state) 
-        plr.CameraMaxZoomDistance = state
-    end
-}
-end
+    AutoFarmTab:Toggle{
+        Name = "AutoClicker",
+        StartingState = false,
+        Description = "Clicks for you",
+        Callback = function(state) 
+            _G.autoClicker = state
+            autoClick()
+        end
+    }
+    MiscTab:Button{
+        Name = "Reset Character",
+        Description = "Resets your character",
+        Callback = function() 
+            wait(0.05)
+            char.Humanoid.Health = 0
+        end
+    }
+    MiscTab:Button{
+        Name = "Kill Roblox",
+        Description = "Destroys roblox instance (just do alt+f4 smh)",
+        Callback = function() 
+            wait(1)
+            game:Shutdown()
+        end
+    }
+    MiscTab:Button{
+        Name = "Destroy NovalineHub",
+        Description = "whyyy :(((!!!",
+        Callback = function() 
+            wait(1)
+            Novaline:Destroy()
+        end
+    }
+    MiscTab:Slider{
+        Name = "Zoom",
+        Default = 130,
+        Min = 1,
+        Max = 10000,
+        Callback = function(state) 
+            plr.CameraMaxZoomDistance = state
+        end
+    }
