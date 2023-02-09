0eeba5e6cb4e6296616a8920fc7539d5f366db56
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-02-09 14:42:10 +0000

    Update 09/02/2023 (Loader)
    hi

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 repeat
 wait()
 until game:IsLoaded()
-print("NovalineHub by Moons#9999 (Thanks to SirWeeb for making this Loader.)")
+print("NovalineHub by Moons#9999")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/novaline-hub/main/Games/".. game.PlaceId .. ".lua"))()
 print("Loading Script for Place ID-".. game.PlaceId)
