e77897d7702734513cb0a5238458cf0916b933dc
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-02-09 13:09:27 +0000

    Update 09/02/2023 (Gas Station Simulator)
    Added Support for Advanced Game Loader

--- Games/10599426741.lua
+++ Games/10599426741.lua
@@ -1,129 +1,113 @@
---Vars
-local inf = math.huge; local defaultNum = 50; local plr = game.Players.LocalPlayer; local char = plr.Character
-
--- Gas Station Simulator (LEGACY)
 local functionNum = 0.00000000000000005
-print('Loading NovalineHub')
-local NovalineConnection = loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/mercury-plus/main/mercury-plus.lua"))()
-
-local Novaline = NovalineConnection:create{
-    Name = 'NovalineHub',
-    Theme = NovalineConnection.Themes.Dark
-}
-
-local AutoFarmTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "AutoFarmTab"
-}
-
-local PlayerTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "Player"
-}
-
-local MiscTab = Novaline:tab{
-    Icon = "rbxassetid://4483362458",
-    Name = "Misc"
-}
-
-_G.autoSprint = true
-_G.autoClean = true
-
-function clean()
-    while _G.autoClean do
-        local args = {
-            [1] = 'Clean',
-            [2] = workspace.Spot.Clean
-        }
-
-        game:GetService('ReplicatedStorage').Remote:FireServer(unpack(args))
-        wait(2)
-    end
-end
-
-function sprint()
-    while _G.autoSprint do
-        local args = {
-            [1] = 'Sprinting',
-            [2] = true
-        }
-
-        game:GetService('ReplicatedStorage').Remote:FireServer(unpack(args))
-        wait(functionNum)
-    end
-    local args = {
-        [1] = 'Sprinting',
-        [2] = false
+    local NovalineConnection = loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/mercury-plus/main/mercury-plus.lua"))()
+    local Novaline = NovalineConnection:create{
+		Name = 'NovalineHub',
+        Theme = NovalineConnection.Themes.Dark
     }
-    game:GetService('ReplicatedStorage').Remote:FireServer(unpack(args))
-end
+    local AutoFarmTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "AutoFarmTab"
+    }
+    local PlayerTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Player"
+    }
+    local MiscTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Misc"
+    }
 
-AutoFarmTab:Toggle{
-    Name = "AutoSprint",
-    StartingState = false,
-    Description = "Sprints for you automatically",
-    Callback = function(state) 
-        _G.autoSprint = state
-        sprint()
-    end
-}
+    _G.autoSprint = true
+    _G.autoClean = true
 
-AutoFarmTab:Toggle{
-    Name = "AutoClean",
-    StartingState = false,
-    Description = "Cleans windows for you automatically",
-    Callback = function(state) 
-        _G.autoClean = state
-        clean()
-    end
-}
+    function clean()
+        while _G.autoClean do
+            local args = {
+                [1] = 'Clean',
+                [2] = workspace.Spot.Clean
+            }
 
-MiscTab:Button{
-    Name = "Reset Character",
-    Description = "Resets your character",
-    Callback = function() 
-        wait(0.05)
-        char.Humanoid.Health = 0
-    end
-}
-
-MiscTab:Button{
-    Name = "Kill Roblox",
-    Description = "Destroys roblox instance (just do alt+f4 smh)",
-    Callback = function() 
-        wait(1)
-        game:Shutdown()
-    end
-}
-
-MiscTab:Slider{
-    Name = "Zoom",
-    Default = 130,
-    Min = 1,
-    Max = 10000,
-    Callback = function(state) 
-        plr.CameraMaxZoomDistance = state
-    end
-}
-
-function godMode()
-    char.Humanoid.WalkSpeed = 155
-    char.Humanoid.JumpPower = 200
-    if _G.godMode == false then
-        char.Humanoid.WalkSpeed = 16
-        char.Humanoid.JumpPower = 50
-    end
-    wait(functionNum)
-end
-
-PlayerTab:Toggle{
-    Name = "GodMode",
-    StartingState = false,
-    Description = "Enables speed and jump powers",
-    Callback = function(state) 
-        _G.godMode = state
-        while true do
-            godMode()
+            game:GetService('ReplicatedStorage').Remote:FireServer(unpack(args))
+            wait(2)
         end
     end
-}
+    function sprint()
+        while _G.autoSprint do
+            local args = {
+                [1] = 'Sprinting',
+                [2] = true
+            }
+
+            game:GetService('ReplicatedStorage').Remote:FireServer(unpack(args))
+            wait(functionNum)
+        end
+        local args = {
+            [1] = 'Sprinting',
+            [2] = false
+        }
+        game:GetService('ReplicatedStorage').Remote:FireServer(unpack(args))
+    end
+
+    AutoFarmTab:Toggle{
+        Name = "AutoSprint",
+        StartingState = false,
+        Description = "Sprints for you automatically",
+        Callback = function(state) 
+            _G.autoSprint = state
+            sprint()
+        end
+    }
+    AutoFarmTab:Toggle{
+        Name = "AutoClean",
+        StartingState = false,
+        Description = "Cleans windows for you automatically",
+        Callback = function(state) 
+            _G.autoClean = state
+            clean()
+        end
+    }
+    MiscTab:Button{
+        Name = "Reset Character",
+        Description = "Respawns your Character",
+        Callback = function() 
+            wait(0.05)
+            char.Humanoid.Health = 0
+        end
+    }
+    MiscTab:Button{
+        Name = "Kill Roblox",
+        Description = "Destroys roblox instance",
+        Callback = function() 
+            wait(1)
+            game:Shutdown()
+        end
+    }
+    MiscTab:Slider{
+        Name = "Zoom",
+        Default = 130,
+        Min = 1,
+        Max = 10000,
+        Callback = function(state) 
+            plr.CameraMaxZoomDistance = state
+        end
+    }
+    function godMode()
+        char.Humanoid.WalkSpeed = 155
+        char.Humanoid.JumpPower = 200
+        if _G.godMode == false then
+            char.Humanoid.WalkSpeed = 16
+            char.Humanoid.JumpPower = 50
+        end
+        wait(functionNum)
+    end
+    PlayerTab:Toggle{
+        Name = "GodMode",
+        StartingState = false,
+        Description = "Enables speed and jump powers",
+        Callback = function(state) 
+            _G.godMode = state
+            while true do
+                godMode()
+            end
+        end
+    }
