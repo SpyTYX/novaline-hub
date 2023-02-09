1869d77e6f7b668dbcedf27fb9aed3520b55fd07
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-02-09 13:32:23 +0000

    Update 09/02/2023
    Added AGL (Advanced Game Loader) :)

--- main.lua
+++ main.lua
@@ -1,9 +1,6 @@
 repeat
-    wait()
+wait()
 until game:IsLoaded()
-
-print("Novaline Hub Loading...")
-
+print("NovalineHub by Moons#9999 (Thanks to SirWeeb for making this Loader.)")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/novaline-hub/main/Games/".. game.PlaceId .. ".lua"))()
-
-print("Loaded script for game id".. game.PlaceId)
+print("Loading Script for Place ID-".. game.PlaceId)
