b5b93d3368712d3cb1ac8ecacdb54f26064a8a09
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-02-09 10:43:08 +0000

    Update 09/02/2023
    Beta Support of Raise a Floppa 2, might be unstable as i havent tested yet.

--- main.lua
+++ main.lua
@@ -3140,10 +3140,29 @@ if game.PlaceId == 3623096087 then
         Name = "Misc"
     }
 
-    _G.autoRep = true
+    _G.autoLift = true
+    _G.autoPunch = true
     _G.godMode = true
 
-    function autoRep()
+    function autoPunch()
+        while _G.autoPunch do
+            local A_1 = {
+                [1] = 'punch',
+                [2] = 'leftHand'
+            }
+            local A_2 = {
+                [1] = 'punch',
+                [2] = 'rightHand'
+            }
+
+            game:GetService('Players').LocalPlayer.muscleEvent:FireServer(unpack(A_1))
+            wait(0.3)
+            game:GetService('Players').LocalPlayer.muscleEvent:FireServer(unpack(A_2))
+            wait(0.3)
+        end
+    end
+
+    function autoLift()
         while _G.autoRep do
             local A_1 = {
                 [1] = 'rep'
@@ -3154,13 +3173,28 @@ if game.PlaceId == 3623096087 then
         end
     end
 
+    AutoFarmTab:Label{
+        Name = 'Important',
+        Description = 'These features require the items to be equipped'
+    }
+
     AutoFarmTab:Toggle{
         Name = "AutoBench",
         StartingState = false,
-        Description = "Automatically reps",
+        Description = "Automatically lifts your dumbell",
         Callback = function(state) 
-            _G.autoRep = state
-            autoRep()
+            _G.autoLift = state
+            autoLift()
+        end
+    }
+
+    AutoFarmTab:Toggle{
+        Name = "AutoPunch",
+        StartingState = false,
+        Description = "Automatically punches for you",
+        Callback = function(state) 
+            _G.autoPunch = state
+            autoPunch()
         end
     }
 
@@ -3333,6 +3367,132 @@ if game.PlaceId == 3823781113 then
         Callback = function(state) 
             _G.godMode = state
             godMode()
+        end
+    }
+end
+
+-- Raise a Floppa 2
+if game.PlaceId == 9772878203 do
+    local functionNum = 0.00000000000000005
+    print('Loading NovalineHub')
+    local NovalineConnection = loadstring(game:HttpGet("https://raw.githubusercontent.com/SpyTYX/mercury-plus/main/mercury-plus.lua"))()
+
+    local Novaline = NovalineConnection:create{
+		Name = 'NovalineHub',
+        Theme = NovalineConnection.Themes.Dark
+    }
+
+    local FloppaTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Flop"
+    }
+
+    local PlayerTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Player"
+    }
+
+    local MiscTab = Novaline:tab{
+        Icon = "rbxassetid://4483362458",
+        Name = "Misc"
+    }
+
+    _G.buyItem = 'Floppa Food'
+    _G.autoRent = true
+
+    function purchase()
+        local A_1 = {
+            [1] = 'Floppa Food',
+            [2] = 'the_interwebs'
+        }
+
+        game:GetService('ReplicatedStorage').Events.Unlock:FireServer(unpack(A_1))
+    end
+
+    function collectRent()
+        game:GetService("ReplicatedStorage").Events:FindFirstChild('Collect Rent'):FireServer()
+    end
+
+    function teleportToRent()
+        while _G.autoRent do
+            game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-78.1193771, 71.3375015, -41.8958969, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+            wait(0.5)
+        end
+    end
+
+    FloppaTab:Button{
+        Name = "Purchase Item",
+        Description = "Purchases Item selected",
+        Callback = function() 
+            wait(0.5)
+            purchase()
+        end
+    }
+
+    local shopSelection = FloppaTab:Dropdown{
+        Name = "Select Item",
+        StartingText = "Food",
+        Description = "Select the item you want",
+        Items = {
+            {"Food", 'Floppa Food'},
+            {"Roommate", 'Roommate'},
+            {"Calender", 'Calender'},
+            {"Poster", 'Cat Poster'},
+            {"Post", 'Stratching Post'},
+            {"Yarn", 'Yarn'},
+            {"Bed", 'Cat Bed'},
+            {"Plant", 'Houseplant'},
+            {"Recipe Book", 'Cook Book'},
+            {"Bowl", 'Baby Bowl'},
+            {"Ms. Floppa", 'Ms. Floppa'},
+            {"Divorce", 'Divorce Papers'},
+            {"Milk", 'Milk Dish'},
+            {"Pizza Recipe", 'Pizza Recipe'},
+            {"Bed", 'Cat Bed'},
+            {"Maid", 'Neko Bed'},
+            {"Bingus", 'Bingus'},
+            {"Homie Sogga", 'Sogga'},
+            {"Maid 2", 'Neko Maid 2.0'},
+            {"Catnip", 'Catnip Plant'},
+            {"Fertilizer", 'Poop Fertilizer'},
+            {"Floppa Sword", 'Floppa Sword'},
+            {"Floppa Shield", 'Floppa Shield'},
+            {"Jinx", 'Jinx'},
+        },
+        Callback = function(Items)
+            _G.buyItem = Items
+            print(_G.buyItem)
+        end
+    }
+
+    FloppaTab:Button{
+        Name = "Get Rent",
+        Description = "Collects Rent",
+        Callback = function() 
+            wait(0.5)
+            collectRent()
+        end
+    }
+
+    FloppaTab:Toggle{
+        Name = "AutoRent",
+        StartingState = false,
+        Description = "Collects rent for you automatically",
+        Callback = function(state) 
+            _G.autoRent = state
+            while _G.autoRent do
+                collectRent()
+                wait(0.5)
+            end
+        end
+    }
+
+    FloppaTab:Toggle{
+        Name = "Teleport to Rent?",
+        StartingState = false,
+        Description = "Teleports to the place where rent spawns while this is on (FOR AFKING)",
+        Callback = function(state) 
+            teleportToRent()
         end
     }
 end
